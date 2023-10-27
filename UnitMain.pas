c5690fd8484c58fa6db1ff3e4cec640c3799eb94
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-10-27 16:30:12 +0100

    Update UnitMain.pas

--- UnitMain.pas
+++ UnitMain.pas
@@ -4,6 +4,8 @@
 {                                               }
 {    @MuminjonGuru                              }
 {                  https://delphi.uz            }
+{ Learn MORE HERE: FMX.PhotoEditorDemo Sample   }
+{                                               }
 {***********************************************}
 
 unit UnitMain;
